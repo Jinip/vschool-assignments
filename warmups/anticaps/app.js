39082dc4ed917474e05887556b8246b75bbf5de4
Jinip <jnperciv@gmail.com>
2017-12-30 21:23:58 -0700

    Updated and cleaned up anticaps

--- warmups/anticaps/app.js
+++ warmups/anticaps/app.js
@@ -1,23 +1,18 @@
-var arr = ["ThIS", "sI", "mEsSed", "Up", "tEXT"];
+//return an array of strings with all uppercase characters converted to lowercase
 
-for (var i = 0; i < arr.length; i++){
-    arr[i] = antiCaps(arr[i]);
-}
-
-console.log(arr);
-
-function antiCaps(str){
-    var newStr = "";
-    for (var i = 0; i < str.length; i++){
-        if (isCaps(str[i])){
-            newStr = newStr + str[i].toLowerCase();
-        } else {
-            newStr = newStr + str[i].toUpperCase();
+function antiCaps(arr){
+    for (var i = 0; i < arr.length; i++){
+        var lowerStr = "";
+        for (var j = 0; j < arr[i].length; j++){
+            if (arr[i][j] === arr[i][j].toLocaleUpperCase()){
+                lowerStr += arr[i][j].toLowerCase();
+            } else {
+                lowerStr += arr[i][j];
+            }
         }
+        arr[i] = lowerStr;
     }
-    return newStr;
+    return arr;
 }
 
-function isCaps(letter) {
-  return letter === letter.toUpperCase();
-}
+console.log(antiCaps(["ThIS", "iS", "mEsSed", "Up", "tEXT"])); // ["this", "is", "messed", "up", "text"]
